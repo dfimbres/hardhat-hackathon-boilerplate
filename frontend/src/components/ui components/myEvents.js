6e0f23a6fe96de31121dbba73d53e85761b7a81b
Alex Goldberg <alexgoldberg@Alexs-MacBook-Pro.local>
2022-03-06 14:02:41 -0700

    updates1

--- frontend/src/components/ui components/myEvents.js
+++ frontend/src/components/ui components/myEvents.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import '../../styles.css'
+import HeaderLoggedIn from './header-signed-in'
 
 export default function myEvents() {
     return (
         <>
+        <HeaderLoggedIn/>
             <div>
                 <h1 className = "center" style={{marginTop: '30px'}}> Coming soon!</h1>
             </div>
